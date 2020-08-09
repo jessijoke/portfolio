0a0637d0f520a6fe4a1b8705b5ba8d0307fa5d55
jessica bradham <jessijoke@yahoo.com>
2020-08-09 02:09:36 -0400

    Customizing

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -15,9 +15,10 @@ class Header extends Component {
       })
     }
 
+    /*<ParticlesBg type="lines" bg={true} />*/
+
     return (
       <header id="home">
-      <ParticlesBg type="circle" bg={true} />
       <nav id="nav-wrap">
          <a className="mobile-btn" href="#nav-wrap" title="Show navigation">Show navigation</a>
 	      <a className="mobile-btn" href="#home" title="Hide navigation">Hide navigation</a>
